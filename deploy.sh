246a7b8b5885fedfcdaee3a59db718aba33ce53d
Eric Lei <elei@seas.upenn.edu>
2025-07-04 15:26:15 -0400

    latest

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+bundle exec jekyll build
+cp -r _site/* leieric.github.io
+cd leieric.github.io
+git add .
+git commit -m "latest"
+git push
+# scp -r _site/* elei@eniac.seas.upenn.edu:/home1/e/elei/public_html
