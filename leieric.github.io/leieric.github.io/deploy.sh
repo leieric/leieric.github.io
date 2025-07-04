8a74edb5517ec97df6f75597a10d894b7ac21a91
Eric Lei <elei@seas.upenn.edu>
2025-07-04 15:47:16 -0400

    Delete leieric.github.io directory

--- leieric.github.io/leieric.github.io/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-bundle exec jekyll build
-cp -r _site/* leieric.github.io
-cd leieric.github.io
-git add .
-git commit -m "latest"
-git push
-# scp -r _site/* elei@eniac.seas.upenn.edu:/home1/e/elei/public_html
